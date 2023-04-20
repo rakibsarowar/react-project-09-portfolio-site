07f09261a4294524015932dee03f2d13c7372632
rakibsarowar <rakibsarowar@gmail.com>
2023-04-20 14:31:27 +0600

    completed navbar

---
+++ src/components/Layout/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Header/Header';
+import './Main.css'
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            
+        </div>
+    );
+};
+
+export default Main;
